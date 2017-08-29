b977f09445282d3b4be7a8edc8cd16887e3dc1c0
cdboeke <cdboeke@gmail.com>
2017-08-29 10:10:08 -0500

    added auto spawn

--- main.js
+++ main.js
@@ -18,6 +18,29 @@ module.exports.loop = function () {
             tower.attack(closestHostile);
         }
     }
+    
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+    
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    console.log('Builders: ' + builders.length);
+    
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Upgraders: ' + upgraders.length);
+
+    if(harvesters.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    else if(upgraders.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgraders'});
+        console.log('Spawning new upgraders: ' + newName);
+    }
+    else if(builders.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+        console.log('Spawning new builder: ' + newName);
+    }
+
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
