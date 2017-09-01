ec9d20689259fcc2528b7d33cffca3dbf52e0d55
cdboeke <cdboeke@gmail.com>
2017-08-31 19:01:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,24 +26,24 @@ module.exports.loop = function () {
     }
     
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
+    //console.log('Harvesters: ' + harvesters.length);
     
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    console.log('Builders: ' + builders.length);
+    //console.log('Builders: ' + builders.length);
     
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    console.log('Upgraders: ' + upgraders.length);
+    //console.log('Upgraders: ' + upgraders.length);
 
     if(harvesters.length < 1) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        var newName = Game.spawns['Spawn1'].createCreep(utilityBody, undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
     else if(upgraders.length < 1) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        var newName = Game.spawns['Spawn1'].createCreep(utilityBody, undefined, {role: 'upgrader'});
         console.log('Spawning new upgraders: ' + newName);
     }
-    else if(builders.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+    else if(builders.length < 2 && Game.rooms.sim.controller.level >= 2) {
+        var newName = Game.spawns['Spawn1'].createCreep(utilityBody, undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
 
